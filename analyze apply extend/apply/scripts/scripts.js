cfd397b979fbcefbeed2387d7ed4be41c8cc11e7
christrilogy <csm29@sunyorange.edu>
2022-11-15 13:35:35 -0500

    ...
    ...

---
+++ analyze apply extend/apply/scripts/scripts.js
@@ -0,0 +1,8 @@
+/* Function to display content */
+
+function content() {
+  var text = document.getElementById("new");
+  text.textContent= "hi. I am a new paragraph that was created with a JavaScript function.";
+  text.style.color = "#c0145f";
+  text.style.fontSize = "2em";
+}
---
+++ analyze apply extend/extend/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+    Student Name: Christian Morgan
+    File Name: script.js
+    Date:11/15/22
+*/
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
